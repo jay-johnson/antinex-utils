7a8a7ee1a25fe20d224a047a5027e76f87eb7d01
jay-johnson <jay.p.h.johnson@gmail.com>
2018-04-02 12:53:39 -0700

    flake8 build

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "antinex_utils"))
 setup(
     name="antinex-utils",
     cmdclass={"test": PyTest},
-    version="1.0.11",
+    version="1.0.12",
     description="AntiNex Utilities for Keras and Tensorflow",
     long_description="" +
     "Standalone AntiNex Utilities for Keras and Tensorflow",
